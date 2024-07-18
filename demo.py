323811294de247c3c6cf53cb4a9ff3bc4ec54256
angls4 <57803800+angls4@users.noreply.github.com>
2024-07-18 20:47:46 +0700

    tes

--- demo.py
+++ demo.py
@@ -520,8 +520,9 @@ print("added middlewaressss")
 certfile = "new_cert.pem"
 keyfile = "new_key.pem"
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-# ssl_context.set_ciphers("ALL:@SECLEVEL=0")
+ssl_context.set_ciphers("ALL:@SECLEVEL=0")
 ssl_context.load_cert_chain(certfile, keyfile)
+ssl_context.set_ciphers("ALL:@SECLEVEL=0")
 print(ssl_context.__dict__)
 async def run_uvicorn():
     config = uvicorn.Config(starlette, host="0.0.0.0",ws='wsproto', http='auto', port=443, log_level="info", ssl_certfile=certfile, ssl_keyfile=keyfile)
